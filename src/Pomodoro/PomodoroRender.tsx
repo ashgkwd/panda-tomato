938fe1fc3f8f11b9f07538a1b1a479c7b84abbf4
ashgkwd <ash.gkwd@gmail.com>
2021-10-07 22:11:07 +0530

    feat: show/hide pause & start, improves styling

--- src/Pomodoro/PomodoroRender.tsx
+++ src/Pomodoro/PomodoroRender.tsx
@@ -2,18 +2,25 @@ import { Button } from "antd";
 import React from "react";
 import Display from "../Countdown/Display";
 import { Pomodorable } from "../types/Pomodorable";
+import "./styles.css";
 
-function PomodoroRender({
-  onToggle,
-  actionLabel,
-  minutes,
-  seconds,
-}: Pomodorable) {
+function PomodoroRender({ onToggle, isPaused, minutes, seconds }: Pomodorable) {
   return (
-    <>
-      <Button onClick={onToggle}>{actionLabel}</Button>
+    <div className="pt-pomodoro">
+      <Button
+        className={`pt-is-paused-${isPaused} pt-pomodoro-pause`}
+        onClick={onToggle}
+      >
+        Pause
+      </Button>
       <Display minutes={minutes} seconds={seconds} />
-    </>
+      <Button
+        className={`pt-is-paused-${isPaused} pt-pomodoro-start`}
+        onClick={onToggle}
+      >
+        Start
+      </Button>
+    </div>
   );
 }
 
