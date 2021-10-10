7e31c2c9a1a050f8324e8b27790034b62426bb36
ashgkwd <ash.gkwd@gmail.com>
2021-10-10 08:09:29 +0530

    ux: uses icon for action buttons 💅

--- src/Pomodoro/PomodoroRender.tsx
+++ src/Pomodoro/PomodoroRender.tsx
@@ -1,38 +1,32 @@
-import { Button, Tooltip } from "antd";
+import { PauseCircleOutlined, PlayCircleOutlined } from "@ant-design/icons";
 import React from "react";
 import Display from "../Countdown/Display";
 import { Pomodorable } from "../types/Pomodorable";
+import ActionButton from "./ActionButton";
 import "./styles.css";
 
 function PomodoroRender({ onToggle, isPaused, minutes, seconds }: Pomodorable) {
   return (
     <div className="pt-pomodoro">
-      <Tooltip
+      <ActionButton
         placement="top"
-        title="Spacebar"
-        className={`pt-visible-${!isPaused}`}
-      >
-        <Button
-          className={`pt-visible-${!isPaused} pt-pomodoro-pause`}
-          onClick={onToggle}
-        >
-          Pause
-        </Button>
-      </Tooltip>
+        tip="Spacebar"
+        visibility={!isPaused}
+        onClick={onToggle}
+        icon={<PauseCircleOutlined />}
+        className="pt-pomodoro-pause"
+        label="Pause"
+      />
       <Display minutes={minutes} seconds={seconds} />
-      <Tooltip
+      <ActionButton
         placement="bottom"
-        title="Spacebar"
-        className={`pt-visible-${isPaused}`}
-      >
-        <Button
-          size="large"
-          className={`pt-visible-${isPaused} pt-pomodoro-start`}
-          onClick={onToggle}
-        >
-          Start
-        </Button>
-      </Tooltip>
+        tip="Spacebar"
+        visibility={isPaused}
+        onClick={onToggle}
+        icon={<PlayCircleOutlined />}
+        className="pt-pomodoro-start"
+        label="Start"
+      />
     </div>
   );
 }
