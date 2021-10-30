7e14256bd8114bbb55a811358851a08579a7a9b1
ashgkwd <ash.gkwd@gmail.com>
2021-10-30 13:23:28 +0530

    chore: clockable supports finish, break and work ⏲

--- src/Pomodoro/useClockable.ts
+++ src/Pomodoro/useClockable.ts
@@ -1,36 +1,77 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { tickSound } from "./tickSound";
 
-function useClockable(startMinutes: number, startSeconds: number) {
+function initialClockSeconds(
+  startMinutes: number,
+  startSeconds: number
+): number {
+  return startMinutes * 60 + startSeconds;
+}
+
+function useClockable(
+  startMinutes: number,
+  startSeconds: number,
+  onFinish?: () => void
+) {
   const [clockSeconds, setClockSeconds] = useState(
-    startMinutes * 60 + startSeconds
+    initialClockSeconds(startMinutes, startSeconds)
   );
   const [minutes, setMinutes] = useState(startMinutes);
   const [seconds, setSeconds] = useState(startSeconds);
   const [isPaused, setIsPaused] = useState(true);
+  const [isReset, setIsReset] = useState(true);
   const interval = useRef<number | undefined>();
 
-  function toggle(): void {
+  const toggle = useCallback((): void => {
     setIsPaused(!isPaused);
+    setIsReset(false);
+  }, [isPaused]);
+
+  const reset = useCallback(
+    (minutes = startMinutes, seconds = startSeconds): void => {
+      setIsReset(true);
+      setIsPaused(true);
+      setMinutes(minutes);
+      setSeconds(seconds);
+      setClockSeconds(initialClockSeconds(minutes, seconds));
+    },
+    [startMinutes, startSeconds]
+  );
+
+  function startWork() {
+    reset(25, 0);
+    toggle();
+  }
+
+  function startBreak() {
+    reset(5, 0);
+    toggle();
   }
 
   useEffect(() => {
     const keyboardShortcutSpace = (ev: KeyboardEvent) => {
-      if (ev.code === "Space") setIsPaused(!isPaused);
+      if (ev.code === "Space") toggle();
+    };
+
+    const keyboardShortcutOptionR = (ev: KeyboardEvent) => {
+      if (ev.code === "KeyR" && ev.altKey) reset();
     };
 
     window.addEventListener("keypress", keyboardShortcutSpace, false);
+    window.addEventListener("keyup", keyboardShortcutOptionR, false);
 
-    return () =>
+    return () => {
       window.removeEventListener("keypress", keyboardShortcutSpace, false);
-  }, [setIsPaused, isPaused]);
+      window.removeEventListener("keyup", keyboardShortcutOptionR, false);
+    };
+  }, [setIsPaused, isPaused, reset, toggle]);
 
   useEffect(() => {
     if (isPaused) return;
     interval.current = window.setInterval(() => {
       const remaining = clockSeconds - 1;
       if (remaining <= 3 && remaining > 0) tickSound();
-      if (remaining < 0) return;
+      if (remaining < 0) return onFinish ? onFinish() : reset();
 
       setClockSeconds(remaining);
       setSeconds(remaining % 60);
@@ -38,9 +79,12 @@ function useClockable(startMinutes: number, startSeconds: number) {
     }, 1000);
 
     return () => clearInterval(interval.current);
-  }, [isPaused, clockSeconds]);
+  }, [isPaused, clockSeconds, onFinish, reset]);
 
-  return [{ minutes, seconds, isPaused }, toggle] as const;
+  return [
+    { minutes, seconds, isPaused, isReset },
+    { toggle, reset: () => reset(), startWork, startBreak },
+  ] as const;
 }
 
 export default useClockable;
