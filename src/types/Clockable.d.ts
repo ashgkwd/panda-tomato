6b50d3275994f6e07211046998d770b4199de81b
ashgkwd <ash.gkwd@gmail.com>
2021-12-28 22:18:04 +0530

    chore: adds types for reset and toggle functions

--- src/types/Clockable.d.ts
+++ src/types/Clockable.d.ts
@@ -4,3 +4,7 @@ export type Clockable = Displayable & {
   isOver: boolean;
   isPaused: boolean;
 };
+
+export type Reset = (minutes?: number, seconds?: number) => void;
+
+export type Toggle = () => void;
